4f6fb5f38a8f2b7f05c68d274b5c570f540369f4
Ryan Lippe <ryanlipp@buffalo.edu>
2022-05-12 15:08:21 -0400

    SQLSchema

---
+++ SQL-Files/CapstoneSquema.sql
@@ -0,0 +1,59 @@
+use master; 
+go
+drop database if exists DisUnemp;
+go
+create database DisUnemp;
+go
+use DisUnemp;
+go
+
+create table SensusData (
+    SensusId    int primary key identity(1, 1),
+    Colmun1      varchar(50) not null,
+    Colmun2      varchar(50) not null,
+    Colmun3      varchar(50) not null,
+    Colmun4      varchar(50) not null,
+);
+
+
+create table DisasterData (
+
+    DisasterId    int primary key identity(1, 1),
+    SBAPhysical      int null,
+    SBAEIDL      int null,
+    SBADisaster      int null,
+    constraint uq_SBADisaster
+        unique (SBADisaster),
+);
+
+
+create table LocationData (
+    LocationId     int primary key identity(1, 1),
+    County       varchar(100) not null,    
+    StateName    varchar(100) not null,
+
+);
+
+create table PredictorData (
+    PlaceTimeId     int primary key identity(1, 1),
+    SensusId     int not null,
+    DisasterId       int not null,
+    LocationId    int null,
+    Year     int not null,
+    UnemployementRate       float not null,
+    SP500    float not null,
+    LoanMinusLoss    float null,
+    VerifiedLossRealEstate float null ,
+    VerifiedLossContent float null , 
+    AmountApprovedRealEstate float null , 
+    ApprovedAmountEIDL float null ,  
+    constraint fk_SensusId
+        foreign key (SensusId)
+        references SensusData(SensusId),
+    constraint fk_DisasterId
+        foreign key (DisasterId)
+        references DisasterData(DisasterId),
+    constraint fk_LocationId
+        foreign key (LocationId)
+        references LocationData(LocationId)
+);
