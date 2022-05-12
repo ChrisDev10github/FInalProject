97e94818f66af8bc580ac13f46442719de7d6b93
Ryan Lippe <ryanlipp@buffalo.edu>
2022-05-12 16:55:49 -0400

    Census

--- SQL-Files/SQLInserts_Joins.sql
+++ SQL-Files/SQLInserts_Joins.sql
@@ -3,9 +3,9 @@ go
 
 --Inserts
 --Change all filenames to name of raw imported table
---Add Sensus Data Column Names
+--Add Census Data Column Names
 
-insert into SensusData (column1, column2, column3, column4)
+insert into CensusData (column1, column2, column3, column4)
 select distinct Category
 from [filename]
 
@@ -17,11 +17,11 @@ insert into LocationData (County, Statename )
 select distinct County, Statename
 from [filename]
 
-insert into PredictorData (DisasterId, SensusID, LocationID, Year, UnemployementRate, SP500, LoanMinusLoss, VerifiedLossRealEstate, VerifiedLossContent, AmountApprovedRealEstate, ApprovedAmountEIDL)
-select DisasterData.DisasterID, SensusData.SensusID, LocationData.LocationId, Year, UnemployementRate, SP500, LoanMinusLoss, VerifiedLossRealEstate, VerifiedLossContent, AmountApprovedRealEstate, ApprovedAmountEIDL
+insert into PredictorData (DisasterId, CensusID, LocationID, Year, UnemployementRate, SP500, LoanMinusLoss, VerifiedLossRealEstate, VerifiedLossContent, AmountApprovedRealEstate, ApprovedAmountEIDL)
+select DisasterData.DisasterID, CensusData.CensusID, LocationData.LocationId, Year, UnemployementRate, SP500, LoanMinusLoss, VerifiedLossRealEstate, VerifiedLossContent, AmountApprovedRealEstate, ApprovedAmountEIDL
 from [filename] as f 
 join DisasterData on f.SBADisaster = DisasterData.SBADisaster
 left join LocationData on f.County = LocationData.County
-join SensusData on f.column1 = SensusData.column1
+join CensusData on f.column1 = CensusData.column1
 
 --May need to test Join statements
