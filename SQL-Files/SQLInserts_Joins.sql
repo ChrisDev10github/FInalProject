fa26174879eb7bd5e4f28f605426c3cb81a9bcfd
Ryan Lippe <ryanlipp@buffalo.edu>
2022-05-17 11:18:00 -0400

    updates

--- SQL-Files/SQLInserts_Joins.sql
+++ SQL-Files/SQLInserts_Joins.sql
@@ -1,27 +1,23 @@
-use DisUnemp;
+use DisasterUnemployment;
 go
 
 --Inserts
 --Change all filenames to name of raw imported table
 --Add Census Data Column Names
 
-insert into CensusData (column1, column2, column3, column4)
-select distinct Category
-from [filename]
 
 insert into DisasterData (SBAPhysical, SBAEIDL, SBADisaster)
 select distinct SBAPhysical, SBAEIDL, SBADisaster
 from [filename]
 
 insert into LocationData (County, Statename )
-select distinct County, Statename
+select distinct County, Statename --Need the destinct combination
 from [filename]
 
-insert into PredictorData (DisasterId, CensusID, LocationID, Year, UnemployementRate, SP500, LoanMinusLoss, VerifiedLossRealEstate, VerifiedLossContent, AmountApprovedRealEstate, ApprovedAmountEIDL)
-select DisasterData.DisasterID, CensusData.CensusID, LocationData.LocationId, Year, UnemployementRate, SP500, LoanMinusLoss, VerifiedLossRealEstate, VerifiedLossContent, AmountApprovedRealEstate, ApprovedAmountEIDL
+insert into PredictorData (DisasterId, CensusID, LocationID, Year, UnemployementRate, SP500, LoanMinusLoss, VerifiedLossRealEstate, VerifiedLossContent, AmountApprovedRealEstate, ApprovedAmountEIDL, PropertyTax, TotalSalariesWages, InterestRevenue, FinesForfeits)
+select DisasterData.DisasterID, LocationData.LocationId, Year, UnemployementRate, SP500, LoanMinusLoss, VerifiedLossRealEstate, VerifiedLossContent, AmountApprovedRealEstate, ApprovedAmountEIDL, PropertyTax, TotalSalariesWages,  InterestRevenue, FinesForfeits --VERIFY All column and table names
 from [filename] as f 
 join DisasterData on f.SBADisaster = DisasterData.SBADisaster
-left join LocationData on f.County = LocationData.County
-join CensusData on f.column1 = CensusData.column1
+join LocationData on f.County = LocationData.County --Modify for County + Sate
 
 --May need to test Join statements
