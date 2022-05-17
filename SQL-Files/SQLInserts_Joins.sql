d6191af320ea63e8a39c67468e62715bc06db808
Ryan Lippe <ryanlipp@buffalo.edu>
2022-05-17 12:27:46 -0400

    updates

--- SQL-Files/SQLInserts_Joins.sql
+++ SQL-Files/SQLInserts_Joins.sql
@@ -1,23 +1,19 @@
-use DisasterUnemployment;
-go
-
 --Inserts
---Change all filenames to name of raw imported table
---Add Census Data Column Names
+--Fix Column Names
 
 
 insert into DisasterData (SBAPhysical, SBAEIDL, SBADisaster)
-select distinct SBAPhysical, SBAEIDL, SBADisaster
-from [filename]
+select distinct [SBA Physical Declaration Number], [SBA EIDL Declaration Number], [SBA Disaster Number]
+from NonNormalizedData
 
-insert into LocationData (County, Statename )
-select distinct County, Statename --Need the destinct combination
-from [filename]
+insert into LocationData (County, StateName)
+select distinct County, State --Need the destinct combination
+from NonNormalizedData
 
-insert into PredictorData (DisasterId, CensusID, LocationID, Year, UnemployementRate, SP500, LoanMinusLoss, VerifiedLossRealEstate, VerifiedLossContent, AmountApprovedRealEstate, ApprovedAmountEIDL, PropertyTax, TotalSalariesWages, InterestRevenue, FinesForfeits)
-select DisasterData.DisasterID, LocationData.LocationId, Year, UnemployementRate, SP500, LoanMinusLoss, VerifiedLossRealEstate, VerifiedLossContent, AmountApprovedRealEstate, ApprovedAmountEIDL, PropertyTax, TotalSalariesWages,  InterestRevenue, FinesForfeits --VERIFY All column and table names
-from [filename] as f 
-join DisasterData on f.SBADisaster = DisasterData.SBADisaster
+insert into PredictorData (DisasterId, CensusID, LocationID, Year, UnemploymentRate, SP500, LoanMinusLoss, VerifiedLossRealEstate, VerifiedLossContent, AmountApprovedRealEstate, ApprovedAmountEIDL, PropertyTax, TotalSalariesWages, InterestRevenue, FinesForfeits)
+select DisasterData.DisasterID, LocationData.LocationId, Year, Unemployement, [Market Value], [Verified LossReal Estate], [Verified Loss Content], [Approved Amount Real Estate], [Approved Amount EIDL], [Property Tax], [Total Salaries & Wages],  [Interest Revenue], [Fines and Forfeits] 
+from NonNormalizedData as f 
+join DisasterData on f.[SBA Disaster Number] = DisasterData.SBADisaster
 join LocationData on f.County = LocationData.County --Modify for County + Sate
 
 --May need to test Join statements
